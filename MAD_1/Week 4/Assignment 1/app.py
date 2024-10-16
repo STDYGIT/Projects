3decb09b216c4b813dc0a26ced808224f85f4a7d
Harsh <harsh.bairagi.05@gmail.com>
2024-10-17 02:48:10 +0530

    Under development

--- MAD_1/Week 4/Assignment 1/app.py
+++ MAD_1/Week 4/Assignment 1/app.py
@@ -1,12 +1,24 @@
-from flask import Flask
 from matplotlib import pyplot as plt
+from flask import Flask, request, render_template, render_template_string
+
 app = Flask(__name__, template_folder='templates')
 
-@app.route("/index", methods=['POST'])
-def index():
-    page = ""
-    pass
+@app.route('/', methods=["POST"])
+def output():
+    data = None
+    id_value = None
+    if request.method == "POST":
+        data = request.form.get("ID")
+        id_value = request.form.get("id_value")
+    if id_value is None or data is None:
+        return render_template("error.html")
+    else:
+        
+        return render_template_string(f"{data}, {id_value}")
 
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
